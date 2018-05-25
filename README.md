a071c1b3bffb33590d1466b47c6b9beb0c73f2a8
Derek <dliu4500@gmail.com>
2018-05-24 23:37:56 -0400

    Icon, title, scaling metrics

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WoTBattleBuddy
+
+Hosted at: <https://dliu4500.github.io/WoTBattleBuddy/>
+
+To do:
+- Consecutive entries can cause graphs to bug
+- Improve scaling on metrics
--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.controller('wotAppController', function($scope, $http)
             document.getElementById("warningMessage").style.display = "none";
             if(response.data.data.length > 1)
             {
+                document.getElementById("warningMessage").innerHTML = "Warning: Multiple players found, showing results for first player";
                 document.getElementById("warningMessage").style.display = "";
                 window.console.warn("Multiple matches found for player name, displaying first result")
             }
@@ -45,17 +46,24 @@ app.controller('wotAppController', function($scope, $http)
             $http.get("https://api.worldoftanks." + $scope.reigon + "/wot/account/info/?application_id=demo&account_id=" + accountID + "&fields=statistics.all")
             .then(function(response){          
                     updateAccountStats(response, accountID);
+                    $scope.graphOffensiveStats();
+                    $scope.graphDefensiveStats();
+                    $scope.graphUtilityStats();
             });
         });
-        $scope.graphOffensiveStats();
-        $scope.graphDefensiveStats();
-        $scope.graphUtilityStats();
+
     }
 
     function updateAccountStats(response, accountID)
     {
         let fields = response.data.data[accountID].statistics.all;
         $scope.battles = fields.battles;
+        if($scope.battles == 0)
+        {
+            document.getElementById("warningMessage").innerHTML = "Warning: Player has no registered games";
+            document.getElementById("warningMessage").style.display = "";
+        }
+
         $scope.winPercentage = parseFloat(fields.wins / fields.battles * 100).toFixed(2);
         $scope.lossPercentage = parseFloat(fields.losses / fields.battles * 100).toFixed(2);
         $scope.drawPercentage = parseFloat(fields.draws / fields.battles * 100).toFixed(2);
@@ -71,18 +79,31 @@ app.controller('wotAppController', function($scope, $http)
         $scope.stunAssistance = parseFloat(fields.stun_assisted_damage / fields.battles_on_stunning_vehicles).toFixed(2);
         $scope.capPoints = parseFloat(fields.capture_points / fields.battles).toFixed(2);
         $scope.defPoints = parseFloat(fields.dropped_capture_points / fields.battles).toFixed(2);
+
+        $scope.effectiveFire = parseFloat(fields.piercings / fields.shots).toFixed(2);
+        $scope.damageRecieved = parseFloat(fields.damage_received / fields.battles).toFixed(2);
+        $scope.damageBlocked = parseFloat(fields.avg_damage_blocked).toFixed(2);
+        $scope.survived = parseFloat(fields.survived_battles / fields.battles).toFixed(2);
+        $scope.stuns = parseFloat(fields.stun_number / fields.battles).toFixed(2);
     }
+    
 
     Chart.defaults.global.legend.display = false;
     $scope.graphOffensiveStats = function () 
     {
+        var expectedTeir = -4.9872+1.22942 * Math.log($scope.battles);
+        if(expectedTeir < 1) expectedTeir = 1;
+        if(expectedTeir > 8.5) expectedTeir = 8.5;
+
+        var scalingFactor = Math.pow(1.25, expectedTeir - 5)
+
         var ctx = document.getElementById("offensiveGraph");
         var myChart = new Chart(ctx, {
             type: 'radar',
             data: {
                 labels: ["Damage", "Damage Ratio", "Destroyed", "Capture Points", "Effective Fire"],
                 datasets: [{
-                    data: [0.5, 0.75, -0.13, 0.2, 0],
+                    data: [($scope.avgDamage / (550 * scalingFactor)) - 1 , $scope.dmgRatio - 1, $scope.destroyed * 1.5 - 1, $scope.capPoints * 2 - 1, $scope.effectiveFire * 2],
                     backgroundColor: "rgba(0, 170, 255, 0.2)",
                     borderColor: "rgba(0, 170, 255, 1)",
                     borderWidth: 1
@@ -105,6 +126,12 @@ app.controller('wotAppController', function($scope, $http)
 
     $scope.graphDefensiveStats = function () 
     {
+        var expectedTeir = -4.9872+1.22942 * Math.log($scope.battles);
+        if(expectedTeir < 1) expectedTeir = 1;
+        if(expectedTeir > 8.5) expectedTeir = 8.5;
+
+        var scalingFactor = Math.pow(1.25, expectedTeir - 5)
+
         var ctx = document.getElementById("defensiveGraph");
         var myChart = new Chart(ctx, {
             type: 'radar',
@@ -112,7 +139,7 @@ app.controller('wotAppController', function($scope, $http)
                 labels: ["Defence Points", "Damage Recieved", "Damage Blocked Ratio", "Survived"],
                 datasets: [{
 
-                    data: [0.1, 0.9, -0.4, -1],
+                    data: [$scope.defPoints * 2 - 1, ($scope.damageRecieved / (300 * scalingFactor)) - 1, $scope.damageBlocked / ((100 * scalingFactor)) - 1, $scope.survived * 5 - 1],
                     backgroundColor: "rgba(0, 170, 255, 0.2)",
                     borderColor: "rgba(0, 170, 255, 1)",
                     borderWidth: 1
@@ -134,6 +161,12 @@ app.controller('wotAppController', function($scope, $http)
 
     $scope.graphUtilityStats = function () 
     {
+        var expectedTeir = -4.9872+1.22942 * Math.log($scope.battles);
+        if(expectedTeir < 1) expectedTeir = 1;
+        if(expectedTeir > 8.5) expectedTeir = 8.5;
+
+        var scalingFactor = Math.pow(1.25, expectedTeir - 5)
+
         var ctx = document.getElementById("utilityGraph");
         var myChart = new Chart(ctx, {
             type: 'radar',
@@ -141,7 +174,7 @@ app.controller('wotAppController', function($scope, $http)
                 labels: ["Spotting Damage", "Stun Damage", "Assistance Damage", "Spotted", "Stuns"],
                 datasets: [{
 
-                    data: [0.1, 0.76, 0, -0.97, 1, -0.45],
+                    data: [($scope.spottingDmg / (200 * scalingFactor)) - 1, ($scope.stunAssistance / (300 * scalingFactor)) - 1, ($scope.assistanceDmg / (50 * scalingFactor)) - 1,  $scope.spotted * 0.75 - 1, $scope.stuns],
                     backgroundColor: "rgba(0, 170, 255, 0.2)",
                     borderColor: "rgba(0, 170, 255, 1)",
                     borderWidth: 1
